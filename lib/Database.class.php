6e55fae4550d4a0f27e2c5bcd07b7800c01a0efd
at384896 <at384896@rwth-aachen.de>
2014-08-27 10:58:24 +0200

    + Template & DatabaseSingelton

---
+++ lib/Database.class.php
@@ -0,0 +1,25 @@
+<?php
+	class Database{
+	
+		private static $dsn = 'mysql:dbname=skriptprog;host=127.0.0.1';
+		private static $user='root';
+		private static $password='';
+		
+		private static $Instance = null;
+		
+		private function __construct(){}
+		
+		public static function getInstance(){
+			
+			if(self::$Instance == null){
+				try {
+					self::$Instance = new PDO(self::$dsn, self::$user, self::$password);
+				} catch (PDOException $e) {
+					echo 'Connection failed: ' . $e->getMessage();
+				}
+			}
+			
+			return self::$Instance;
+		}
+	}
+?>
---
+++ lib/Template.class.php
@@ -0,0 +1,21 @@
+<?php
+	class Template{
+	
+		private $map = array();
+	
+		public function assign($key, $value){
+			$key = '{$' . $key . '}'; 
+			$this->map[$key] = $value;
+		}
+		
+		public function display($documentName){
+			$htmlText= file_get_contents($documentName);
+			
+			foreach($this->map as $key => $value){
+				$htmlText = str_replace($key, $value, $htmlText);
+			}
+			
+			echo $htmlText;
+		}
+	}
+?>
