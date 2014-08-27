81a3513df9f59499dfaef43b8ca10c6bdce4e03f
FelixHolzaepfel-Stein <abbel193@gmail.com>
2014-08-27 14:27:37 +0200

    Database auf User aufgeteilt und korrigiert

--- lib/Database.class.php
+++ lib/Database.class.php
@@ -2,14 +2,14 @@
 	class Database{
 	
 		private static $dsn = 'mysql:dbname=projekt_db;host=127.0.0.1';
-		private static $user='root';
-		private static $password='';
+		private static $$user='root';
+		private static $$password='';
 		
 		private static $Instance = null;
 		
 		private function __construct(){}
 		
-		private static function getInstance(){
+		public static function getInstance(){
 			
 			if(self::$Instance == null){
 				try {
@@ -22,58 +22,7 @@
 			return self::$Instance;
 		}
 		
-		public static function checkPasswordForUser(user,password){
-			$dbh = self::getInstance();
-			$sth = $dbh->prepare('Select Password from benutzer where Name=:input or Email=:input');
-			$sth->bindParam(':input',user);
-			$sth->execute();
-			$row = $sth->fetch();
-			if(password_verify(password,$row['Password'])){
-				return true;
-			} else {
-				return false;
-			}
-		}
 		
-		public static function registerUser(user,email,password){
-			try {
-				$dbh = self::getInstance();
-				$sth = $dbh->prepare('INSERT INTO user(`Name`, `Password`, `Email`) VALUES (:name,:pw,:email)');
-				$sth->bindParam(':name', user);
-				$sth->bindParam(':pw', password_hash(password, PASSWORD_BCRYPT));
-				$sth->bindParam(':email', email;
-				$sth->execute();
-				return true;
-			} catch (PDOException $e) {
-				return false;
-			}
-		}
-		
-		public static function userExists(name){
-			$dbh = self::getInstance();
-			$sth = $dbh->prepare('Select count(*) FROM user WHERE ´Name´ = :name');
-			$sth->bindParam(':name',name);
-			$sth->execute();
-			$sth->storeResult();
-			if($sth->num_rows > 0 ){
-				return true;
-			}else{
-				return false;
-			}
-		}
-		
-		public static function emailExists(email){
-			$dbh = self::getInstance();
-			$sth = $dbh->prepare('Select count(*) FROM user WHERE ´Email´ = :email');
-			$sth->bindParam(':email',email);
-			$sth->execute();
-			$sth->storeResult();
-			if($sth->num_rows > 0 ){
-				return true;
-			}else{
-				return false;
-			}
-		}
 		
 		
 		
---
+++ lib/User.class.php
@@ -0,0 +1,71 @@
+<?php
+require './Database.class.php';
+
+class User{
+	public static function checkpasswordForuser($user,$password){
+			$dbh = Database::getInstance();
+			$sth = $dbh->prepare('Select password from benutzer where Name=:input or email=:input');
+			$sth->bindParam(':input',$user);
+			$sth->execute();
+			$row = $sth->fetch();
+			if($password_verify($password,$row['password'])){
+				return true;
+			} else {
+				return false;
+			}
+		}
+		
+		public static function registerUser($user,$email,$password){
+			try {
+				$dbh = Database::getInstance();
+				$sth = $dbh->prepare('INSERT INTO user(`Name`, `password`, `email`) VALUES (:name,:pw,:email)');
+				$sth->bindParam(':name', $user);
+				$sth->bindParam(':pw', password_hash($password, password_BCRYPT));
+				$sth->bindParam(':email', $email);
+				$sth->execute();
+				return true;
+			} catch (PDOException $e) {
+				return false;
+			}
+		}
+		
+		public static function changepassword($user,$password){
+			try {
+				$dbh = Database::getInstance();
+				$sth = $dbh->prepare('UPDATE ´user´ SET password = :pw WHERE ´name´ = :name');
+				$sth->bindParam(':name', $user);
+				$sth->bindParam(':pw', password_hash($password, password_BCRYPT));
+				$sth->execute();
+				return true;
+			} catch (PDOException $e) {
+				return false;
+			}
+		}
+		
+		public static function userExists($name){
+			$dbh = Database::getInstance();
+			$sth = $dbh->prepare('Select count(*) FROM user WHERE ´Name´ = :name');
+			$sth->bindParam(':name',$name);
+			$sth->execute();
+			$sth->storeResult();
+			if($sth->num_rows > 0 ){
+				return true;
+			}else{
+				return false;
+			}
+		}
+		
+		public static function emailExists($email){
+			$dbh = Database::getInstance();
+			$sth = $dbh->prepare('Select count(*) FROM user WHERE ´email´ = :email');
+			$sth->bindParam(':email',$email);
+			$sth->execute();
+			$sth->storeResult();
+			if($sth->num_rows > 0 ){
+				return true;
+			}else{
+				return false;
+			}
+		}
+}
+?>
