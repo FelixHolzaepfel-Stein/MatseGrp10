8dd5d4adea48da333df56a60e5f439c08cb62e4d
NiklasRoth <niklas.roth300@web.de>
2014-08-27 15:45:41 +0200

    register.php

--- main/register.php
+++ main/register.php
@@ -9,18 +9,28 @@ if(isset($_SESSION['logged_in'])){
 	}
 else {
 	$tpl = new Template();
-	$tpl->display('templates/register.tpl');
+	$tpl->display('../templates/register.tpl');
+	if(isset($_SESSION['error']) && isset($_POST['tmp'])){
+			echo $_SESSION['error'];
+	}
 	if(isset($_POST['tmp'])){
 		if(User::userExists($_POST['Name']) && User::emailExists($_POST['Email'])){
 			if(User::userExists($_POST['Name'])){
-			
-			User::registerUser($_POST['Name'],$_POST['Email'],$_POST['Password']);
-			}
-			else {
-			$_SESSION['error']= 'Name bereits vergeben.'
+				if(User::emailExists($_POST['Email'])){
+					if($_POST['Password1']===$_POST['Password2']){
+						User::registerUser($_POST['Name'],$_POST['Email'],$_POST['Password1']);
+					} else {
+						$_SESSION['error']= 'Passwoerter sind ungleich.';
+					}
+				} else {
+					$_SESSION['error']= 'Email bereits vergeben.';
+				}
+			} else {
+				$_SESSION['error']= 'Name bereits vergeben.';
 			}
 		} else {
-			$_SESSION['error']= 'Name und Email bereits vergeben.'
+			$_SESSION['error']= 'Name und Email bereits vergeben.';
 		}
 	}
+}
 ?>
