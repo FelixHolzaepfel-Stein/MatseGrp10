3aab2b9a3a5d60b753c28e9ef5f39b8cec5d9c50
MonaScholl <scholl.mona@googlemail.com>
2014-08-28 15:17:46 +0200

    Alle divs ausgeblendet per default

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,13 @@
 $(function() {
+
+	$('#divMain').hide();
+	$('#divAllianz').hide();
+	$('#divGame').hide();
+	$('#divMail').hide();
+	$('#divProfil').hide();
+	$('#divRanking').hide();
+	$('#divShop').hide();
+	
 	$('#main').click(function () {
 					$.ajax('main.php')
 						.done(function (data) { 
