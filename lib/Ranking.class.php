6ac80c374e4f64527f50f46616873f616c380976
eneshoyladi <hoyladi@154-200.eduroam.rwth-aachen.de>
2014-08-28 10:11:26 +0200

    Ranking.class.php angepasst
    Ranking.class.php angepasst

--- lib/Ranking.class.php
+++ lib/Ranking.class.php
@@ -1,44 +1,52 @@
 <?php
 
 
-require('Database.class.php');
+require("Database.class.php");
 
 class Ranking
 {
 	
-	static  $recordAmountsPerPage = 10;
+	 
 
-	public static fetchRanking($lowerBound, $upperBound)
+	public static function fetchRanking($lowerBound, $upperBound)
 	{
 		$db= Database::getInstance();
-		$statement = $db->prepare('Select * from user LIMIT :lowerBound, :upperBound ORDER BY points DESC');
+		$db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+		$db->setAttribute(PDO::ATTR_EMULATE_PREPARES, FALSE);
+		$statement = $db->prepare("Select * from benutzer ORDER BY points DESC LIMIT :lowerBound, :upperBound;");
 
 		//Assign nicht vergesen !!!!
-		$statement->bindParam(':lowerBound', $lowerBound);
-		$statemnet->bindParam(':upperBound', $upperBound);
+		$statement->bindParam(":lowerBound", $lowerBound, PDO::PARAM_INT);
+		$statement->bindParam(":upperBound", $upperBound, PDO::PARAM_INT);
 		$statement->execute();
 
 
 
-		$returnHtml = '<table>';
-		$returnHtml .= '<tr>';
-		$returnHtml .= '<th>Name</th><th>Punktzahl</th>';
-		$returnHtml .= '<tr>';
+		$returnHtml = "<table>";
+		$returnHtml .= "<thead>";
+		$returnHtml .= "<tr>";
+		$returnHtml .= "<th>Name</th><th>Punktzahl</th>";
+		$returnHtml .= "</tr>";
+		$returnHtml .= "</thead>";
+		$returnHtml .= "<tbody id='rankingTable'>";
 
+		$recordAmountPerPage = 10;
 		$count = $lowerBound+1;
 
-		while($row = $statement->fetch())
-		{
+		while($row = $statement->fetch(PDO::FETCH_ASSOC))
+		{	
+
 			
-			$pageNumber = $count % self::$recordAmountPerPage;
-			$returnHtml .= '<tr><td class="'.$pageNumber'">'.$row[name].'</td><td>'.$row[points].'</td><div id="'.$row[id].'</div></tr>';
+			$pageNumber = $count % $recordAmountPerPage;
+			$returnHtml .= "<tr id='".$row['ID']."'><td class='".$pageNumber." '>".$row['Name']."</td><td>".$row['Points']."</td><td class='description'>".$row['Description']."</td></tr>";
 			$count++;
 		}
 
+		$returnHtml .= "</tbody>";
+		$returnHtml .= "</table>";
 		return $returnHtml;
 
 			}
 	}
 
 
-?>
