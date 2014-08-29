dc2b898676c4e3f7009f49c4e5faa0c7f14b9785
eneshoyladi <hoyladi@178-026.eduroam.rwth-aachen.de>
2014-08-29 09:02:32 +0200

    Ranking.js angepasst

--- scripts/ranking.js
+++ scripts/ranking.js
@@ -1,12 +1,14 @@
 $(function() {
-	$('#ranking').click(function () {
-
+	$('#ranking').click(function () 
+	{
+					$('#pageNumber').html(1);
 					
 					$.get('ranking.php', { recordsLoaded : 0 , upperBound: 1 }, function(data){ 
 
 						//alert(data);
 						$('#divRanking').hide();
-						$('#divRanking').html(data);
+						$('#divGame').hide();
+						$('#rankingTable').html(data);
 						$('#divMain').hide();
 						$('#divProfil').hide();
 						$('#divMail').hide();
@@ -34,6 +36,84 @@ $(function() {
 
 
 				});
+	
+	// Funktionen für das Paging
+	
+	(function ()
+	{
+		var pageNumber = parseInt($('#pageNumber').html());
+	
+		if(pageNumber === 1)
+		{
+			$('#backButton').hide();
+		}
+		
+		$('#backButton').click(function(evt){
+			
+				var pageNumber = parseInt($('#pageNumber').html());
+				
+				$('.'+pageNumber).hide();
+				$('.'+(pageNumber-1)).show();
+				$('#pageNumber').html((--pageNumber));
+				if(pageNumber === 1)
+				{
+					$('#backButton').hide();
+				}
+				
+		
+		});
+		
+		$('#nextButton').click(function(evt){
+			
+			var pageNumber = parseInt($('#pageNumber').html());
+			var upperBound = pageNumber+1;
+			if($('tr.'+ upperBound).length!== 0)
+			{
+				$('tr.'+pageNumber).hide();
+				$('tr.'+(upperBound)).show();
+				$('#backButton').show();
+				$('#pageNumber').html(pageNumber+1);
+				
+			}
+			else
+			{
+				$.get('ranking.php', {recordsLoaded : pageNumber , upperBound: upperBound}, function(data)
+				{
+					if(data === "" ||  data === " " || data == "  ")
+					{
+						return;
+					}
+
+					$('#rankingTable').append(data);
+					
+					$('#rankingTable tr').hover(
+						function()
+						{
+							
+							$('#'+$(this).attr('id')+' td.description').fadeIn();
+						},
+						
+						 function()
+						 {
+						 	$('#'+$(this).attr('id')+' td.description').fadeOut();
+						 });
+					$('tr.'+pageNumber).hide();
+				
+					$('#pageNumber').html(upperBound);
+					$('tr.'+(upperBound)).show();
+					$('#backButton').show();
+					
+					
+				});
+			}
+			
+		
+		
+		
+		});
+	
+	
+	})();
 
 
 
